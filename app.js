0cdc913184934666523a1cf8addc1a78f4ae7c7f
kayneluo <chienweiluo@gmail.com>
2024-10-04 19:12:29 +0800

    add google endpoint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
+const querystring = require('querystring');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
@@ -11,26 +11,33 @@ app.use(bodyParser.json());
 // Line Bot Webhook Endpoint
 app.post('/webhook', (req, res) => {
   const events = req.body.events;
-
+  console.log(events, 'events');
   events.forEach(async (event) => {
-    if (event.type === 'message' && event.message.type === 'text') {
-      const userMessage = event.message.text;
-      
-      if (userMessage === '✅') {
-        // 處理收到的✅回覆，例如更新 Google Sheets
-        await updateGoogleSheet(event.source.userId);
+    const { message } = event;
+    const { type, data } = message;
+    if (type === 'postback') {
+      const jsonQuery = querystring.parse(data) || {};
+      if (jsonQuery.action === 'checked') {
+        const rowNumber = jsonQuery.rowNumber;
+        try {
+          const response = await axios.post('https://script.google.com/macros/s/AKfycbxSokQ9woEqX550VBvUt4qS6vW4-1kKJtHPGmFdbCApJ_uX4tR5D3hajKNkN7OSp5Jp/exec', {
+            rowNumber: rowNumber
+          });
+  
+          if (response.data.result === 'success') {
+            console.log(`Row ${rowNumber} updated successfully.`);
+          } else {
+            console.error(`Failed to update row: ${response.data.error}`);
+          }
+        } catch (error) {
+          console.error(`Error calling Google Apps Script: ${error.message}`);
+        }
       }
     }
   });
 
   res.sendStatus(200);
 });
-
-// 假設的更新 Google Sheets 函數
-async function updateGoogleSheet(userId) {
-  // 在這裡實現您對 Google Sheets 的更新邏輯
-  console.log(`Received response from user: ${userId}`);
-}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
